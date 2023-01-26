3ff06b1deaaa399f383ed1f63122c92aa45e5755
Varun <varunsesh@gmail.com>
2023-01-26 18:50:22 +0530

    Refactor + Adding two more balls

--- scripts/main.js
+++ scripts/main.js
@@ -6,51 +6,107 @@ canvas.height = window.innerHeight - 100;
 var simMinWidth = 20;
 var cScale = Math.min(canvas.width, canvas.height)/simMinWidth;
 
-var dt = 1/100;
+var dt = 1/60;
 var g = {x:0, y:-10};
 
-var ball = {
-    pos : {x:0.2, y:0.2}, 
-    radius : 0.2,
-    velocity:{x:5, y:6}
+
+class ballObj{
+    constructor(pos, m, vel, r){
+        if(pos === null){
+            this.pos = {x:0, y:0}
+        }
+        if(m === null){
+            this.m = 0.0;
+        }
+        if(vel === null){
+            this.vel = {x:0, y:0};
+        }
+        if(r === null){
+            r = 1.0;
+        }
+
+        this.pos=pos;
+        this.m = m;
+        this.vel = vel;
+        this.r = r;
+    }
 }
 
+
 function cX(pos){
-    return ball.pos.x*cScale;
+    return pos.x*cScale;
 }
 
 function cY(pos){
-    return canvas.height - ball.pos.y*cScale ;
+    return canvas.height - pos.y*cScale ;
 }
 
-function draw(){
-    c.clearRect(0,0,canvas.width, canvas.height);
-    c.fillStyle = "#FF0000";
+nballs = 2;
 
+var balls = [];
+
+function spawnBalls(){
+    var dx = 1.0
+    var ball1 = new ballObj({x:1.0, y:1.0}, 1.0, {x:10.0, y:12.0}, 0.5)
+    var ball2 = new ballObj({x:2.0, y:1.0}, 1.0, {x:5.0, y:12.0}, 0.5)
+    balls.push(ball1);
+    balls.push(ball2);
+}
+
+function computeCollisions(){
+
+}
+
+function circle(colour, radius, pos){
+    c.fillStyle = colour;
     c.beginPath();
-    c.arc(cX(ball.pos), cY(ball.pos), ball.radius*cScale, 0, 2*Math.PI);
+    c.arc(cX(pos), cY(pos), radius*cScale, 0, 2*Math.PI);
     c.fill();
 }
 
+var ball = new ballObj({x:1.0, y:1.0}, 1.0, {x:5.0, y:6.0}, 0.75);
+
+function draw(){
+    c.clearRect(0,0, canvas.width, canvas.height);
+    circle('#FF0000', ball.r, ball.pos);
+
+    for (var i = 0; i<balls.length; i++){
+        circle('#00FF00', balls[i].r, balls[i].pos);
+    }
+}
 
 function simulate(){
+    for (var i = 0; i<balls.length; i++){
+        balls[i].vel.x += g.x*dt;
+        balls[i].vel.y += g.y*dt;
 
-    ball.velocity.x += g.x*dt;
-    ball.velocity.y += g.y*dt;
+        balls[i].pos.x += balls[i].vel.x*dt;
+        balls[i].pos.y += balls[i].vel.y*dt;
+
+        if(balls[i].pos.x*cScale < 0 || balls[i].pos.x*cScale > canvas.width){
+            balls[i].vel.x = -balls[i].vel.x;
+        }
+
+        if(balls[i].pos.y < 0){
+            balls[i].pos.y = 0;
+            balls[i].vel.y = -balls[i].vel.y;
+        }
+
+    }
+
+    ball.vel.x += g.x*dt;
+    ball.vel.y += g.y*dt;
     
-
-    ball.pos.x += ball.velocity.x*dt;
-    ball.pos.y += ball.velocity.y*dt;
+    ball.pos.x += ball.vel.x*dt;
+    ball.pos.y += ball.vel.y*dt;
 
     if(ball.pos.x*cScale < 0 || ball.pos.x*cScale > canvas.width){
-        ball.velocity.x = -ball.velocity.x;
-        console.log(ball.velocity.x);
-        
+        ball.vel.x = -ball.vel.x;
     }
 
     if(ball.pos.y < 0){
         ball.pos.y = 0;
-        ball.velocity.y = -ball.velocity.y;
+        ball.vel.y = -ball.vel.y;
     }
 }
 
@@ -60,5 +116,6 @@ function update(){
     requestAnimationFrame(update);
 }
 
+spawnBalls();
 update();
 
